4a518868aa1a62eccc6d7bbe3cd3831cda8e72bd
shirbhate-saurabh <saurabh311292@gmail.com>
2020-03-19 20:09:37 +0530

    updation done

---
+++ doctors-database/doctor_db/doctor_db.py
@@ -0,0 +1,37 @@
+#Doctor's Table
+
+doctor = {
+          1: {'first_name':'Rajesh', 'last_name': 'Singh'},
+          2: {'first_name':'Sonal', 'last_name': 'Dutt'},
+          3: {'first_name':'J.S.', 'last_name': 'Chandra'}
+         }
+
+#Patient Table##########PENDING#################
+
+patient = {
+           11:{'first_name':'Saurabh', 'last_name':'S', 'address':'New Panvel','city':'Navi Mumbai','state':'Maharashtra'},
+           12:{'first_name':'Shreyas', 'last_name':'N', 'address':'Ghatkopar','city':'Delhi','state':'Delhi'}
+          }
+
+#Bill Table
+
+bill = {
+        111:{'bill_date':'15-10-2018', 'bill_status':'Pending'},
+        112:{'bill_date':'20-10-2018', 'bill_status':'Paid'}
+       }
+
+#payment Table
+
+payment_receipt = {
+                   1010:{'payment_date':'15-10-2018', 'payment_method':'Cash', 'payment_amount':2000},
+                   1016:{'payment_Date':'20-10-2018', 'payment_method':'Card', 'pay_amount':5000}
+                  }
+          
+
+#Appointment Table
+appointment_details = {
+                       '15-10-2018':{'appointment_time':'10:30 AM', 'appointment_duration':'00:30:00',
+                                     'appointment_reason':'Report Checking'},
+                       '20-10-2018':{'appointment_time':'06:30 PM', 'appointment_duration':'01:00:00',
+                                     'appointment_reason':'Routine Checkup'}
+                      }
