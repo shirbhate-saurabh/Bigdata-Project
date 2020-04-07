7adb31d865690988e968ee14f9135f57c54035a8
shirbhate-saurabh <saurabh311292@gmail.com>
2020-04-08 01:43:51 +0530

    changes done

--- doctors-database/classes.py
+++ doctors-database/classes.py
@@ -1,48 +1,60 @@
 """creating classes for doctor database in which base class is "Person" and other classes are
-   "doctor", "patient", "appointment""""
+   "doctor", "patient", "appointment\""""
 
 from collections import defaultdict
 from datetime import datetime
 
 
 class Person:
-    def __init__(self,first_name,last_name):
+    
+    def __init__(self,first_name,last_name,address):
         
         self.first_name = ['Saurabh','shreays',"Akshay","Ritesh"]
         
         self.last_name = ["S","Shirbhate","Kale","Shivkar"]
         
+        self.address = ["panvel","ghatkopar","vashi","bhingari"]
+        
         
 class Doctor:
-    def __init__(self):
-        pass
-        
+
     def doctor_id_generator():
         temp = defaultdict(lambda: len(temp) + 1)
         temp1 = [temp[ele] for ele in self.first_name]
         doctor_id = str(temp1)
         print(doctor_id)
-        
-        
+
+
 class Patient:
-    def __init__(self):
-        pass
-    
+
     def patient_id_generator():
         temp = defaultdict(lambda: len(temp) + 1000)
         temp1 = [temp[ele] for ele in self.first_name]
         patient_id = str(temp1)
-        print(patient_id)
-    
+        return patient_id
+
 class Aappointment:
-    def __init__(self):
-        pass
-    
+
     def appointment_id_generator():
         temp = datetime.now().timestatmp()
         temp1 = str(temp).replace('.','')
         appointment_id = temp1
+        return appointment_id
+        
+        
+"""class main():
+    rohan = Person("Rohan","shirke","nerul")
     
+    print("this is programe :")
+    print("details are as mentioned below")
+    print("Enter name :", rohan.first_name.append())
+    print("patient_id id : ")
+    print("appointment ID is : ",rohan.appointment_id_generator())
+    
+    
+if __name__ == "__main__":
+    main()
+"""
     
     
             
