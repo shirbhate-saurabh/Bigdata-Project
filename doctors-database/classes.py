dc7272d5c42cda05f0a1f51f946a25fa3fc991af
shirbhate-saurabh <saurabh311292@gmail.com>
2020-04-07 20:31:44 +0530

    file added

---
+++ doctors-database/classes.py
@@ -0,0 +1,47 @@
+"""creating classes for doctor database in which base class is "Person" and other classes are
+   "doctor", "patient", "appointment""""
+
+from collections import defaultdict
+from datetime import datetime
+
+
+class Person:
+    def __init__(self,first_name,last_name):
+        
+        self.first_name = ['Saurabh','shreays',"Akshay","Ritesh"]
+        
+        self.last_name = ["S","Shirbhate","Kale","Shivkar"]
+        
+        
+class Doctor:
+    def __init__(self):
+        pass
+        
+    def doctor_id_generator():
+        temp = defaultdict(lambda: len(temp) + 1)
+        temp1 = [temp[ele] for ele in self.first_name]
+        doctor_id = str(temp1)
+        print(doctor_id)
+        
+        
+class Patient:
+    def __init__(self):
+        pass
+    
+    def patient_id_generator():
+        temp = defaultdict(lambda: len(temp) + 1000)
+        temp1 = [temp[ele] for ele in self.first_name]
+        patient_id = str(temp1)
+        print(patient_id)
+    
+class Aappointment:
+    def __init__(self):
+        pass
+    
+    def appointment_id_generator():
+        appointment_id = datetime.now()
+    
+    
+    
+            
+        
